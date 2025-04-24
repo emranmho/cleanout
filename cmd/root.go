1867ba2cc2c07675d097e12e7411fea072334951
Mohammodullah Emran <63514104+emranmho@users.noreply.github.com>
2025-04-24 09:16:43 +0600

    init comit

---
+++ cmd/root.go
@@ -0,0 +1,16 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var rootCmd = &cobra.Command{
+	Use:   "tempcleaner",
+	Short: "A CLI tool to clean temp/cache files older than a specified age",
+	Long: `TempCleaner is a CLI utility for scanning and removing old temp/cache files.
+It supports dry-run mode, custom paths, and age threshold.`,
+}
+
+func Execute() {
+	// Execute the root command
+	cobra.CheckErr(rootCmd.Execute())
+
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/emranmho/tempcleaner/cmd"
+)
+
+func main() {
+
+	// Execute the root command
+	cmd.Execute()
+}
